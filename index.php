763e72e03083edda54d15d7328f67ab03c2bf251
B1ll5zn <whyguiih@gmail.com>
2026-05-19 16:05:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - SEMP</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-form">
+            <h1>Login</h1>
+            <form action="#">
+                <div class="input-group">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" id="usuario" value="" required>
+                </div>
+                
+                <div class="input-group">
+                    <label for="senha">Senha</label>
+                    <input type="password" id="senha" value="" required>
+                </div>
+                
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+
+        <div class="login-brand">
+            <img src="img/SENAI_Branco.png" alt="Logo SENAI">
+        </div>
+    </div>
+
+    <footer>
+        Copyright &copy; Threeeo (Gabriel Artuso, Guilherme Brandalize e Larissa Gazoli) 2026. Todos os direitos reservados.
+    </footer>
+</body>
+</html>
