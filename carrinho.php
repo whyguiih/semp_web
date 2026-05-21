36129d66083ee0ce847f65981c47711bacdbf220
whyguiih <whyguiih@gmail.com>
2026-05-21 14:27:07 -0300

    Refactor access control in multiple pages to restrict certain features to specific user account levels, enhancing security and user experience.

--- carrinho.php
+++ carrinho.php
@@ -20,11 +20,11 @@ if (!is_array($produtos_carrinho)) $produtos_carrinho = [];
         <a href="estoque.php"><img src="img/lista.png" alt="Lista"></a>
         <a href="carrinho.php"><img src="img/carrinho.png" alt="Carrinho"></a>
         
-        <?php if ($_SESSION['nivel_conta'] == '1' || $_SESSION['nivel_conta'] == '2'): ?>
+        <?php if ($_SESSION['nivel_conta'] == '1'): ?>
             <a href="autorizar_pedidos.php"><img src="img/controle.png" alt="Controlo"></a>
         <?php endif; ?>
 
-        <?php if ($_SESSION['nivel_conta'] == '1'): ?>
+        <?php if ($_SESSION['nivel_conta'] == '1' || $_SESSION['nivel_conta'] == '2'): ?>
             <a href="cadastro_produto.php"><img src="img/lupa.png" alt="Cadastro"></a>
         <?php endif; ?>
         
